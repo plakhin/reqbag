76553886ab71c35c16eb867ce2f0629931eaa189
Stanislav Plakhin <plakhin@gmail.com>
2024-06-25 21:36:49 +0700

    adjust `Request` Model and migration to include analysis attributes/columns

---
+++ database/migrations/2024_06_25_062901_add_is_analysis_requested_and_analysis_columns_to_requests_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('requests', function (Blueprint $table) {
+            $table->text('analysis')->nullable()->after('raw');
+            $table->boolean('is_analysis_requested')->default(false)->after('analysis');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('requests', function (Blueprint $table) {
+            $table->dropColumn(['analysis', 'is_analysis_requested']);
+        });
+    }
+};

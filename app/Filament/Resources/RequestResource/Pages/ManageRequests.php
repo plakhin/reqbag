cf1d64f434e6dc5e71b119579089eff92c73be52
Stanislav Plakhin <plakhin@gmail.com>
2024-07-11 21:21:07 +0700

    show `createRequestBagAction` Modal by default in case no Bags exist

--- app/Filament/Resources/RequestResource/Pages/ManageRequests.php
+++ app/Filament/Resources/RequestResource/Pages/ManageRequests.php
@@ -13,34 +13,51 @@ class ManageRequests extends ManageRecords
 {
     protected static string $resource = RequestResource::class;
 
+    public function __construct()
+    {
+        if (Bag::doesntExist()) {
+            $this->defaultAction = 'createRequestBagAction';
+        }
+    }
+
     protected function getHeaderActions(): array
     {
         return [
-            Action::make('Create request bag')
-                ->icon('heroicon-m-shopping-bag')
-                ->modalIcon('heroicon-m-shopping-bag')
-                ->modalDescription(
-                    'To store incoming requests you need to create at least one bag (subdomain).'
-                    .' Once the bag is created, all requests coming to '
-                    .config('app.scheme').'{bag}.'.config('app.central_domain')
-                    .' will be stored to the database and displayed on this page grouped by bag.'
-                )
-                ->form([
-                    TextInput::make('slug')->label('Bag subdomain')->required()->unique((new Bag())->getTable(), 'slug'),
-                ])
-                ->modalSubmitActionLabel('Create')
-                ->action(function (array $data): void {
-                    Bag::create($data);
-                }),
-            Action::make('Delete request bags')
-                ->icon('heroicon-m-x-circle')
-                ->modalIcon('heroicon-m-x-circle')
-                ->color('danger')
-                ->outlined()
-                ->form([Select::make('bags')->multiple()->required()->options(Bag::pluck('slug', 'id'))])
-                ->requiresConfirmation()
-                ->action(fn (array $data): int => Bag::destroy($data['bags']))
-                ->hidden(Bag::doesntExist()),
+            $this->createRequestBagAction(),
+            $this->deleteRequestBagsAction(),
         ];
+    }
+
+    protected function createRequestBagAction(): Action
+    {
+        return Action::make('Create request bag')
+            ->icon('heroicon-m-shopping-bag')
+            ->modalIcon('heroicon-m-shopping-bag')
+            ->modalDescription(
+                'To store incoming requests you need to create at least one bag (subdomain).'
+                .' Once the bag is created, all requests coming to '
+                .config('app.scheme').'{bag}.'.config('app.central_domain')
+                .' will be stored to the database and displayed on this page grouped by bag.'
+            )
+            ->form([
+                TextInput::make('slug')->label('Bag subdomain')->required()->unique((new Bag())->getTable(), 'slug'),
+            ])
+            ->modalSubmitActionLabel('Create')
+            ->action(function (array $data): void {
+                Bag::create($data);
+            });
+    }
+
+    protected function deleteRequestBagsAction(): Action
+    {
+        return Action::make('Delete request bags')
+            ->icon('heroicon-m-x-circle')
+            ->modalIcon('heroicon-m-x-circle')
+            ->color('danger')
+            ->outlined()
+            ->form([Select::make('bags')->multiple()->required()->options(Bag::pluck('slug', 'id'))])
+            ->requiresConfirmation()
+            ->action(fn (array $data): int => Bag::destroy($data['bags']))
+            ->hidden(Bag::doesntExist());
     }
 }

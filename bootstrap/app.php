674a02a8985c0543f9d39f9b864ddd2fa2cf9b90
Stanislav Plakhin <plakhin@gmail.com>
2024-07-19 18:42:46 +0700

    move `{bag}` domain route group definition to avoid default Middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,14 +1,21 @@
 <?php
 
+use App\Http\Middleware\SaveRequest;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: fn () => Route::domain('{bag}.'.config('app.central_domain'))->group(
+            fn () => Route::any('/{path?}', fn () => 'ok')
+                ->where('path', '.*')
+                ->middleware(SaveRequest::class)
+        )
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,1 @@
 <?php
-
-use App\Http\Middleware\SaveRequest;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
-use Illuminate\Support\Facades\Route;
-
-Route::domain('{bag}.'.config('app.central_domain'))->group(function () {
-    Route::any('/{path?}', fn () => 'ok')
-        ->where('path', '.*')
-        ->middleware(SaveRequest::class)
-        ->withoutMiddleware(VerifyCsrfToken::class);
-});

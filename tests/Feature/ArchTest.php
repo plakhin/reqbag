d604d8f544a7c57a8cd5a92171cd0b1b4ae4acbd
Stanislav Plakhin <plakhin@gmail.com>
2024-10-05 08:39:29 +0700

    fix Arch tests

--- tests/Feature/ArchTest.php
+++ tests/Feature/ArchTest.php
@@ -2,5 +2,10 @@
 
 use App\Providers\Filament\AdminPanelProvider;
 
-arch()->preset()->laravel()->ignoring(AdminPanelProvider::class); //@phpstan-ignore-line
-arch()->preset()->security(); //@phpstan-ignore-line
+arch()->preset()->laravel()->ignoring( //@phpstan-ignore-line: PHPStan doesn't recognize preset()
+    AdminPanelProvider::class, //uses Filament naming convention
+);
+
+arch()->preset()->security()->ignoring( //@phpstan-ignore-line: PHPStan doesn't recognize preset()
+    'parse_str', //parse_str() considered safe since PHP 8.0
+);
